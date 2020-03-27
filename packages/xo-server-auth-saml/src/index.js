71ba1684c5b577515c1b3361aee776e094c6757e
phil <phil@flexsystem.com>
2020-03-27 20:01:06 +0800

    *

--- packages/xo-server-auth-saml/src/index.js
+++ packages/xo-server-auth-saml/src/index.js
@@ -61,7 +61,7 @@ class AuthSamlXoPlugin {
       ...conf,
 
       // must match the callback URL
-      path: '/signin/saml/callback',
+      path: '/xo/signin/saml/callback',
     }
 
     if (loaded) {
