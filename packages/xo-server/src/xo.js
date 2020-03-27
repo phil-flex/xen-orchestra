21524278ec88f6e89a2288c9a732ed2036d164c6
phil <phil@flexsystem.com>
2020-03-27 16:32:31 +0800

    *

--- packages/xo-server/src/xo.js
+++ packages/xo-server/src/xo.js
@@ -27,9 +27,6 @@ const warn = (...args) => {
 }
 
 // ===================================================================
-
-const log = createLogger('xo:xo')
-
 @mixin(mapToArray(mixins))
 export default class Xo extends EventEmitter {
   constructor(config) {
@@ -169,8 +166,6 @@ export default class Xo extends EventEmitter {
         }
       },
       error => {
-        log.error('HTTP request error', { error })
-
         if (!res.headersSent) {
           res.writeHead(500)
         }
