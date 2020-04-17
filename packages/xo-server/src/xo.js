eb1d870584eac46bd7c69574e311915cae7523fa
Julien Fontanet <julien.fontanet@isonoe.net>
2020-04-17 17:09:40 +0200

    feat(xo-server/_handleHttpRequest): send directly string/Buffer results

--- packages/xo-server/src/xo.js
+++ packages/xo-server/src/xo.js
@@ -152,7 +152,9 @@ export default class Xo extends EventEmitter {
     }).then(
       result => {
         if (result != null) {
-          if (typeof result.pipe === 'function') {
+          if (typeof result === 'string' || Buffer.isBuffer(result)) {
+            res.end(result)
+          } else if (typeof result.pipe === 'function') {
             result.pipe(res)
           } else {
             res.end(JSON.stringify(result))
